e88cfe6ba81f3d88e83fcbc6577aca49f94a2e98
Fernando Chicaiza <fchicaiza.g1990@gmail.com>
2024-03-01 09:17:24 -0500

    task(todo) Generate Models and Migrations - add model "todo" using sequelize-cli command - add the userId field in order to relate the "todo" entity to the "user" entity

---
+++ src/database/migrations/20240301141112-create-todo.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      isComplete: {
+        type: Sequelize.BOOLEAN
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Todos');
+  }
+};
